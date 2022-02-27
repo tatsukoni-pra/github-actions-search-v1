a4b45888cda24a0dd9fb2249aaa0b0f76eaa631e
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2022-02-27 21:16:51 +0900

    feat: テスト失敗検証の戻し

--- src/tests/Feature/Controllers/TestApiControllerTest.php
+++ src/tests/Feature/Controllers/TestApiControllerTest.php
@@ -54,7 +54,7 @@ class TestApiControllerTest extends TestCase
 
         // Assert
         $res->assertStatus(201);
-        // $res->assertStatus(202);
+        $res->assertStatus(202);
         $this->assertSame(1, Test::count());
         $this->assertDatabaseHas('tests', [
             'name' => 'hoge1',
